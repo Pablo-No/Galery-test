4108f4af970a9336a8e818af1e0547a763108235
Pablo-No <65043486+Pablo-No@users.noreply.github.com>
2020-07-09 22:45:52 +0200

    =Initial commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,37 @@
+var images = [
+  "assets/images/1.jpg",
+  "assets/images/2.jpg",
+  "assets/images/3.jpg"
+];
+var previousImage = document.getElementById("previousImage");
+var nextImage = document.getElementById("nextImage");
+var slider = document.getElementById("slider");
+var num = 0
+//For almost all browsers
+if (previousImage.addEventListener) {
+  previousImage.addEventListener("click", prev);
+  nextImage.addEventListener("click", next);
+}//For IE 8 or older
+else if (previousImage.attachEvent) {
+  previousImage.attachEvent("onclick", prev);
+  nextImage.attachEvent("onclick", next);
+}
+else {
+  //Nothing can be done
+}
+
+function next() {
+  num++;
+  if(num >= images.length) {
+    num = 0;
+  }
+  slider.src = images[num];
+  }
+
+function prev() {
+  num--;
+  if(num < 0) {
+    num = images.length-1;
+  }
+  slider.src = images[num];
+}
