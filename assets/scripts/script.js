8fe3bf7327ff7f44f21d062bf9de1759389c5682
Pablo-No <65043486+Pablo-No@users.noreply.github.com>
2020-07-10 16:54:32 +0200

    Add ./ to relative paths

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,7 @@
 var images = [
-  "assets/images/1.jpg",
-  "assets/images/2.jpg",
-  "assets/images/3.jpg"
+  "./assets/images/1.jpg",
+  "./assets/images/2.jpg",
+  "./assets/images/3.jpg"
 ];
 var alt = [
   "Sunflowers",
