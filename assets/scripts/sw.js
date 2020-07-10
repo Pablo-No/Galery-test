06e06fca48132b26812b569ef03528857dd25641
Pablo-No <65043486+Pablo-No@users.noreply.github.com>
2020-07-10 13:30:34 +0200

    Task 1 — Cache Static Assets

--- assets/scripts/sw.js
+++ assets/scripts/sw.js
@@ -1,20 +1,21 @@
-importScripts('/cache-polyfill.js');
-
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('airhorner').then(function(cache) {
-     return cache.addAll([
-       '/Privado1/index.html',
-       '/Privado1/assets/images/1.jpg',
-       '/Privado1/assets/images/2.jpg',
-       '/Privado1/assets/images/3.jpg',
-       '/Privado1/assets/scripts/script.js',
-       '/Privado1/assets/styles/style.css',
-       '/Privado1/assets/styles/fonts/NotoSans/NotoSans-Regular.woff2',
-       '/Privado1/assets/styles/fonts/NotoSans/NotoSans-Italic.woff2',
-       '/Privado1/assets/styles/fonts/NotoSans/NotoSans-Bold.woff2',
-       '/Privado1/assets/styles/fonts/NotoSans/NotoSans-BoldItalic.woff2'
-     ]);
-   })
- );
+var CACHE_NAME = 'my-offline-cache';
+var urlsToCache = [
+  '/Privado1/index.html',
+  '/Privado1/assets/images/1.jpg',
+  '/Privado1/assets/images/2.jpg',
+  '/Privado1/assets/images/3.jpg',
+  '/Privado1/assets/scripts/script.js',
+  '/Privado1/assets/styles/style.css',
+  '/Privado1/assets/styles/fonts/NotoSans/NotoSans-Regular.woff2',
+  '/Privado1/assets/styles/fonts/NotoSans/NotoSans-Italic.woff2',
+  '/Privado1/assets/styles/fonts/NotoSans/NotoSans-Bold.woff2',
+  '/Privado1/assets/styles/fonts/NotoSans/NotoSans-BoldItalic.woff2'
+];
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
