97326f84b91df71e208b2ee710ab075cc08df762
Pablo-No <65043486+Pablo-No@users.noreply.github.com>
2020-07-10 16:39:36 +0200

    Try to fix relative paths in sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
 var CACHE_NAME = 'my-offline-cache';
 var urlsToCache = [
-  'index.html',
-  'assets/images/1.jpg',
-  'assets/images/2.jpg',
-  'assets/images/3.jpg',
-  'assets/scripts/script.js',
-  'assets/styles/style.css',
-  'assets/styles/fonts/NotoSans/NotoSans-Regular.woff2',
-  'assets/styles/fonts/NotoSans/NotoSans-Italic.woff2',
-  'assets/styles/fonts/NotoSans/NotoSans-Bold.woff2',
-  'assets/styles/fonts/NotoSans/NotoSans-BoldItalic.woff2'
+  './index.html',
+  './assets/images/1.jpg',
+  './assets/images/2.jpg',
+  './assets/images/3.jpg',
+  './assets/scripts/script.js',
+  './assets/styles/style.css',
+  './assets/styles/fonts/NotoSans/NotoSans-Regular.woff2',
+  './assets/styles/fonts/NotoSans/NotoSans-Italic.woff2',
+  './assets/styles/fonts/NotoSans/NotoSans-Bold.woff2',
+  './assets/styles/fonts/NotoSans/NotoSans-BoldItalic.woff2'
 ];
 
 self.addEventListener('install', function(event) {
