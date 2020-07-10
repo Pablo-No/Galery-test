8f412e4c79acc2a56cd533076bef289364e24f34
Pablo-No <65043486+Pablo-No@users.noreply.github.com>
2020-07-10 13:35:01 +0200

    Change urls to relative paths

--- assets/scripts/sw.js
+++ assets/scripts/sw.js
@@ -1,15 +1,15 @@
 var CACHE_NAME = 'my-offline-cache';
 var urlsToCache = [
-  '/Privado1/index.html',
-  '/Privado1/assets/images/1.jpg',
-  '/Privado1/assets/images/2.jpg',
-  '/Privado1/assets/images/3.jpg',
-  '/Privado1/assets/scripts/script.js',
-  '/Privado1/assets/styles/style.css',
-  '/Privado1/assets/styles/fonts/NotoSans/NotoSans-Regular.woff2',
-  '/Privado1/assets/styles/fonts/NotoSans/NotoSans-Italic.woff2',
-  '/Privado1/assets/styles/fonts/NotoSans/NotoSans-Bold.woff2',
-  '/Privado1/assets/styles/fonts/NotoSans/NotoSans-BoldItalic.woff2'
+  '../index.html',
+  '../assets/images/1.jpg',
+  '../assets/images/2.jpg',
+  '../assets/images/3.jpg',
+  '../assets/scripts/script.js',
+  '../assets/styles/style.css',
+  '../assets/styles/fonts/NotoSans/NotoSans-Regular.woff2',
+  '../assets/styles/fonts/NotoSans/NotoSans-Italic.woff2',
+  '../assets/styles/fonts/NotoSans/NotoSans-Bold.woff2',
+  '../assets/styles/fonts/NotoSans/NotoSans-BoldItalic.woff2'
 ];
 self.addEventListener('install', function(event) {
   event.waitUntil(
