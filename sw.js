94f901fe1d299187f8bf78e59edc0650f7c232ee
Pablo-No <65043486+Pablo-No@users.noreply.github.com>
2020-07-10 21:37:08 +0200

    Try using await rather than then inside the async function
    Try the answer on this post 
    https://stackoverflow.com/questions/57905153/serviceworkers-fetch-object-that-was-not-a-response-was-passed-to-respondwit/57912824#57912824

--- sw.js
+++ sw.js
@@ -38,3 +38,23 @@ self.addEventListener('fetch', function(event) {
     )
   );
 })
+event.respondWith((async () => {
+  const cachedResponse = await caches.match(event.request);
+  if (cachedResponse) {
+    return cachedResponse;
+  }
+
+  const response = await fetch(event.request);
+
+  if (!response || response.status !== 200 || response.type !== 'basic') {
+    return response;
+  }
+
+  if (ENABLE_DYNAMIC_CACHING) {
+    const responseToCache = response.clone();
+    const cache = await caches.open(DYNAMIC_CACHE)
+    await cache.put(event.request, response.clone());
+  }
+
+  return response;
+})());
